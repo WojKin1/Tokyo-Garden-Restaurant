fe9033005d38c67d56f3a0862da4012f9d175b31
DominikDeszczka <92813525+DominikDeszczka@users.noreply.github.com>
2025-04-23 21:12:48 +0200

    Add files via upload

---
+++ BL/ObslugaUzytkownikow.cs
@@ -0,0 +1,19 @@
+﻿using IBL;
+using IDAL;
+using Model;
+
+namespace BL
+{
+    public class ObslugaUzytkownikow : IObslugaUzytkownik
+    {
+        public IEnumerable<Uzytkownik> PobierzPosortowaneUzytkownikow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PoliczUzytkownikow()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BL/ObsługaAlergenow.cs
@@ -0,0 +1,19 @@
+﻿using IBL;
+using IDAL;
+using Model;
+
+namespace BL
+{
+    public class ObsługaAlergenow : IObslugaAlergenow
+    {
+        public IEnumerable<Alergeny> PobierzPosortowaneAlergeny()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PoliczAlergeny()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
