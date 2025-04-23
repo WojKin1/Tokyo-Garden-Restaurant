e3b06d30eb9b79b18d750627708aa653084133ff
DominikDeszczka <92813525+DominikDeszczka@users.noreply.github.com>
2025-04-23 21:15:06 +0200

    Add files via upload

---
+++ IDAL/IAlergenyRepository.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+
+namespace IDAL
+{
+    public interface IAlergenyRepository : IDisposable
+    {
+        IEnumerable<Alergeny> GetAlergeny();
+        Alergeny GetAlergenyByID(int alergenId);
+        void InsertAlergen(Alergeny alergen);
+        void DeleteAlergen(int alergenId);
+        void UpdateAlergen(Alergeny alergen);
+        void Save();
+    }
+}
+
---
+++ IDAL/IUzytkownikRepository.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+
+namespace IDAL
+{
+     public interface IUzytkownikRepository : IDisposable
+    {
+        IEnumerable<Uzytkownik> GetUzytkownik();
+        Uzytkownik GetUzytkownikByID(int uzytkownikID);
+        void InsertUzytkownik(Uzytkownik uzytkownik);
+        void DeleteUzytkownik(int uzytkownikID);
+        void UpdateUzytkownik(Uzytkownik uzytkownik);
+        void Save();
+    }
+}
+
