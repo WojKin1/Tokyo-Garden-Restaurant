a8b4623d510bbbf45edba2313f563393f1ed4e4c
DominikDeszczka <92813525+DominikDeszczka@users.noreply.github.com>
2025-04-23 21:07:56 +0200

    Add files via upload

---
+++ IDAL/IAdresyRepository.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+
+namespace IDAL
+{
+        public interface IAdresyRepository : IDisposable
+        {
+            IEnumerable<Adresy> GetAdresy();
+            Adresy GetAdresyByID(int adresId);
+            void InsertAdres(Adresy adres);
+            void DeleteAdres(int adresId);
+            void UpdateAdres(Adresy adres);
+            void Save();
+        }
+   
+}
---
+++ Model/Adresy.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    public class Adresy
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(25)]
+        public string miasto { get; set; }
+        [MaxLength(10)]
+        public string nr_domu { get; set; }
+        [MaxLength(25)]
+        public string nr_mieszkania { get; set; }
+        [MaxLength(50)]
+        public string ulica { get; set; }
+    }
+}
