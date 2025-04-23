47cc76956e221f5c18215c250a43857a0a9968e6
DominikDeszczka <92813525+DominikDeszczka@users.noreply.github.com>
2025-04-23 21:16:13 +0200

    Add files via upload

---
+++ Model/Alergeny.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    public class Alergeny
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(25)]
+        public string nazwa_alergenu { get; set; }
+        [MaxLength(100)]
+        public string opis_alergenu { get; set; }
+    }
+}
---
+++ Model/Uzytkownik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    public class Uzytkownik
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(25)]
+        public string nazwa_uzytkownika { get; set; }
+        [MaxLength(15)]
+        public string haslo { get; set; }
+        [MaxLength(12)]
+        public string telefon { get; set; }
+        [MaxLength(50)]
+        public int typ_uzytkownika { get; set; }
+
+    }
+}
