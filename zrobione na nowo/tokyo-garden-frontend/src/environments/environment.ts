3fc095b17bcaf0c5c1bdfbe4b002d6c6b77c32ea
DominikDeszczka <92813525+DominikDeszczka@users.noreply.github.com>
2025-09-17 16:56:57 +0200

    Add files via upload

--- zrobione na nowo/tokyo-garden-frontend/src/environments/environment.ts
+++ zrobione na nowo/tokyo-garden-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // Zmień port/protokół jeśli u Ciebie inny – chodzi o URL lokalnego API
-  apiUrl: 'https://localhost:5280/api'
+  apiUrl: 'http://localhost:5280/api'
 };
